e9c6c81b68b90bd67877171dacee74ebd3b4fbe2
Ayange Felix <ayangefelix8@gmail.com>
2022-07-07 12:15:10 +0100

    print the result

--- canvas.py
+++ canvas.py
@@ -15,3 +15,8 @@ commandListB = re.split(r'\s',commandB)
 print(commandListB)
 x1,y1,x2,y2 = int(commandListB[1]),int(commandListB[2]),int(commandListB[3]),int(commandListB[4])
 myCanvas[y1, x1:x2+1] = np.ones((x2-x1+1,), dtype=int)
+
+chars_ascii1 = np.array(['.','x'], dtype="U1")[myCanvas]
+print(chars_ascii1)
+strings_f = chars_ascii1.view('U' + str(chars_ascii1.shape[1])).flatten()
+print( "\n".join(strings_f))
